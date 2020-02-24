af1dc5ced78424e9a73aebfa35f2986a8154ea2c
Becky <xpeng62@gatech.edu>
2020-02-23 19:20:20 -0500

    improve interface efficiency -becky

--- monopoly_simulator/interface.py
+++ monopoly_simulator/interface.py
@@ -34,6 +34,9 @@ class Interface(object):
         self.state_space = []
         self.masked_actions = []
         self.move_actions = []
+        self.action_space_num = 1 + 22 + 28 + 28 + 1
+        self.action_space = []
+        self.site_space = []
 
     #state_space = 28+22+n+n+2 = 56
     def board_to_state(self, current_board):
@@ -94,6 +97,14 @@ class Interface(object):
         return self.state_space
 
     def get_masked_actions(self, allowable_actions, param, current_player):
+        '''
+        This function is to transfer allowable actions to vector/array.
+        :param allowable_actions: a set. allowed actions from player.py.
+        :param param: dict. parameters for allowed actions from play.py.
+        :param current_player: player class.
+        :return: masked_actions, a list.
+        '''
+
         masked_actions = []
         #1 first denotes the action -> buy or not buy
         if buy_property in allowable_actions:
@@ -125,48 +136,67 @@ class Interface(object):
         for space in self.board_owned:
             masked_actions.append(1) if space in potentials else masked_actions.append(0)
 
+        # 1 action: always allowed : conclude the actions = skip = do nothing.
+        masked_actions.append(1)
+
         self.masked_actions = masked_actions
         return self.masked_actions
 
+    #action_# is 80
     def action_num2vec(self, action):
-        actions_vec = [0 for i in range(1+ 22 + 28 + 28)]
+        actions_vec = [0 for i in range(self.action_space_num)]
         actions_vec[int(action)] = 1
         return actions_vec
 
-    #action space 1+22+28+28 = 79
-    def action_space(self, current_board, current_player):
-        action_space = []
-        site_space = []
-        # 1 first denotes the action -> buy or not buy
-        action_space.append(buy_property)
-        site_space.append(current_board['location_objects'][self.board_state[current_player.current_position]])
+    #action space 1+22+28+28+1 = 80
+    def get_action_space(self, current_board, current_player):
+        '''
 
-        # 22 improve property
-        for space in self.board_building:
-            site_space.append(current_board['location_objects'][space])
-        for i in range(len(self.board_building)):
-            action_space.append(improve_property)
+        :param current_board: the gameboard with history.
+        :param current_player: the player moving now: in our game, is player_1.
+        :return:
+        '''
+        if self.site_space:
+            self.site_space[0] = current_board['location_objects'][self.board_state[current_player.current_position]]
+        else:
 
-        #28 actions_allowed_mortgage
-        for space in self.board_owned:
-            site_space.append(current_board['location_objects'][space])
-        for i in range(len(self.board_owned)):
-            action_space.append(mortgage_property)
+            # 1 first denotes the action -> buy or not buy
+            self.action_space.append(buy_property)
+            self.site_space.append(current_board['location_objects'][self.board_state[current_player.current_position]])
 
-        # 28 actions: free mortgage
-        for space in self.board_owned:
-            site_space.append(current_board['location_objects'][space])
-        for i in range(len(self.board_owned)):
-            action_space.append(free_mortgage)
+            # 22 improve property
+            for space in self.board_building:
+                self.site_space.append(current_board['location_objects'][space])
+            for i in range(len(self.board_building)):
+                self.action_space.append(improve_property)
 
-        return action_space,site_space
+            #28 actions_allowed_mortgage
+            for space in self.board_owned:
+                self.site_space.append(current_board['location_objects'][space])
+            for i in range(len(self.board_owned)):
+                self.action_space.append(mortgage_property)
+
+            # 28 actions: free mortgage
+            for space in self.board_owned:
+                self.site_space.append(current_board['location_objects'][space])
+            for i in range(len(self.board_owned)):
+                self.action_space.append(free_mortgage)
+
+        return self.action_space, self.site_space
 
         #take into array/vector and output the actions
-    actions_vector_default = [1] + [0 for i in range(22+28+28)]
+    actions_vector_default = [1] + [0 for i in range(79)]
+
     def vector_to_actions(self, current_board, current_player, actions_vector=actions_vector_default):
+        '''
+        Take into array/vector of actions from agent and output the actions which are taken into env
+        :param current_board: dict.
+        :param current_player: class.
+        :param actions_vector: list.
+        :return: a action function.
+        '''
         move_actions = []
-        action_space, site_space = self.action_space(current_board, current_player)
-        print('actions_vector =====> ', actions_vector)
+        self.action_space, self.site_space = self.get_action_space(current_board, current_player)
         try:
             actions_vector = actions_vector.tolist()
         except AttributeError:
@@ -174,16 +204,16 @@ class Interface(object):
 
         for i,action in enumerate(actions_vector):
             if action == 1:
-                move_actions.append((action_space[i], site_space[i]))
+                move_actions.append((self.action_space[i], self.site_space[i]))
 
         self.move_actions = move_actions
-
+        return self.move_actions
         #####becky - may be deleted##########
-        allowed_types = [location.UtilityLocation, location.RailroadLocation, location.RealEstateLocation]
-        if type(move_actions[0][1]) in allowed_types:
-            return self.move_actions
-        else:
-            return []
+        # allowed_types = [location.UtilityLocation, location.RailroadLocation, location.RealEstateLocation]
+        # if type(move_actions[0][1]) in allowed_types:
+        #     return self.move_actions
+        # else:
+        #     return []
 
 
 
