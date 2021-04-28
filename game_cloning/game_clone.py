cfebfe21d274062b5cee1e02529edbe83fa1aefb
Jonathan Balloch <jon.balloch@gmail.com>
2021-04-28 19:10:21 -0400

    working example integrated with gc novelty detector

--- game_cloning/game_clone.py
+++ game_cloning/game_clone.py
@@ -11,12 +11,13 @@ class GameClone():
         Args:
             rules_manual: json of rules
         """
-        self.rules = {}
+        self.rules = dict()
         self.rules_manual_json = rules_manual_json
         self.red_button = False
         if rules_manual_json:
             self._process_manual()
 
+        self.state_stats = dict()
         # a record is a sequence that grows over time
         # a sequence is a set that is ordered
         # a set is a group of like objects
@@ -73,70 +74,75 @@ class GameClone():
     def gc_detect_novelty(self, gameboard_message):
         data_dict_from_server = json.loads(gameboard_message)
 
-        novelty_properties = {}
+        if self.red_button:
+            return True, {'message': 'novelty alredy detected!'}
 
-        ## TODO balloch: dummy
-        if not self.red_button and np.random.rand() > 0.99:
+        novelty, novelty_properties = self.check_novelty()
+        if novelty:
             self.red_button = True
-            novelty_properties['type'] = 'dummy'
-            novelty_properties['trigger'] = 'random'
 
-        return self.red_button, novelty_properties
+        return self.novelty, novelty_properties
 
 
-    def update_novelty_detector(self, state_stats, gameboard):
+    def update_novelty_detector(self, parsed_json, subtree=None):
         # tree_search
-        for key in gameboard:
-            if (not gameboard[key]) or (key in ('cards', 'history', 'players')):
+        if subtree is None:
+            subtree = self.state_stats
+        for key in parsed_json:
+            if (not parsed_json[key]) or (key in ('cards', 'history', 'players')):
                 continue
-            elif type(gameboard[key]) is dict:
-                if key not in state_stats:
-                    state_stats[key] = {}
-                state_stats[key] = self.update_novelty_detector(state_stats[key],
-                                                           gameboard[key])
+            elif type(parsed_json[key]) is dict:
+                if key not in subtree:
+                    subtree[key] = {}
+                subtree[key] = self.update_novelty_detector(parsed_json[key], subtree[key])
 
-            elif type(gameboard[key]) is list:
-                if type(gameboard[key][-1]) is dict:  # history # TODO balloch: untested
-                    if key not in state_stats:
-                        state_stats[key] = []
-                    for idx, subdict in enumerate(gameboard[key]):
-                        if idx > (len(state_stats[key]) - 1):
-                            state_stats[key].append(self.update_novelty_detector({}, subdict))
+            elif type(parsed_json[key]) is list:
+                if type(parsed_json[key][-1]) is dict:  # history # TODO balloch: untested
+                    if key not in subtree:
+                        subtree[key] = []
+                    for idx, subjson in enumerate(parsed_json[key]):
+                        if idx > (len(subtree[key]) - 1):
+                            subtree[key].append(self.update_novelty_detector(subjson, {}))
                         else:  # TODO: if subtree the same, add count, if not, insert? frankly list should become a tree
-                            state_stats[key][idx] = self.update_novelty_detector(state_stats[key][idx],
-                                                                            gameboard[key][idx])
-                elif type(gameboard[key][-1]) is list:  # hardcoding this because die sequence only list of lists
-                    if key not in state_stats:
-                        state_stats[key] = {'length': 0, 'data': [{}, {}]}
-                    if state_stats[key]['length'] == len(gameboard[key]):
+                            subtree[key][idx] = self.update_novelty_detector(parsed_json[key][idx], subtree[key][idx])
+                elif type(parsed_json[key][-1]) is list:  # hardcoding this because die sequence only list of lists
+                    if key not in subtree:
+                        subtree[key] = {'length': 0, 'data': [{}, {}]}
+                    if subtree[key]['length'] == len(parsed_json[key]):
                         pass  # do nothing
                         # print('No dice change updates')
                     else:
-                        state_stats[key]['length'] += 1
-                        for idx, item in enumerate(gameboard[key][-1]):
-                            if item not in state_stats[key]['data'][idx]:
-                                state_stats[key]['data'][idx][item] = 1
+                        subtree[key]['length'] += 1
+                        for idx, item in enumerate(parsed_json[key][-1]):
+                            if item not in subtree[key]['data'][idx]:
+                                subtree[key]['data'][idx][item] = 1
                             else:
-                                state_stats[key]['data'][idx][item] += 1
+                                subtree[key]['data'][idx][item] += 1
                 else:  # (str, bool int float)
-                    if key not in state_stats:
-                        state_stats[key] = {tuple(gameboard[key]): 1}
-                    elif tuple(gameboard[key]) not in state_stats[key]:
-                        state_stats[key][tuple(gameboard[key])] = 1  # TODO: I think we need this somewhere else too?
+                    if key not in subtree:
+                        subtree[key] = {tuple(parsed_json[key]): 1}
+                    elif tuple(parsed_json[key]) not in subtree[key]:
+                        subtree[key][tuple(parsed_json[key])] = 1  # TODO: I think we need this somewhere else too?
                     else:
-                        state_stats[key][tuple(gameboard[key])] += 1
+                        subtree[key][tuple(parsed_json[key])] += 1
 
             else:  # (str, bool, int, float)
-                if key not in state_stats:
-                    state_stats[key] = {gameboard[key]: 1}
-                elif gameboard[key] not in state_stats[key]:
-                    state_stats[key][gameboard[key]] = 1  # TODO: I think we need this somewhere else too?
+                if key not in subtree:
+                    subtree[key] = {parsed_json[key]: 1}
+                elif parsed_json[key] not in subtree[key]:
+                    subtree[key][parsed_json[key]] = 1  # TODO: I think we need this somewhere else too?
                 else:
-                    state_stats[key][gameboard[key]] += 1
-        return state_stats
+                    try:
+                        subtree[key][parsed_json[key]] += 1
+                    except TypeError:
+                        print('key: ', key, ' - parsed_json[key]: ', parsed_json[key])
+        return subtree
 
 
-    def check_novelty(self, state_stats, parsed_json):
+    def check_novelty(self, parsed_json, subtree=None):
+        if subtree is None:
+            subtree = self.state_stats
+
         novelty = False
         novelty_properties = {}
         for key in parsed_json:
@@ -145,53 +151,53 @@ class GameClone():
 
             if (not parsed_json[key]) or (key in ('cards', 'history', 'players', 'locations')):
                 continue
-            elif key not in state_stats:
+            elif key not in subtree:
                 novelty = True
                 break
             elif type(parsed_json[key]) is dict:
-                novelty, novelty_properties = self.check_novelty(state_stats[key],
+                novelty, novelty_properties = self.check_novelty(subtree[key],
                                                                  parsed_json[key])
             elif type(parsed_json[key]) is list:
                 if type(parsed_json[key][-1]) is dict:  # list of dicts - history # TODO balloch: untested
-                    for idx, subdict in enumerate(parsed_json[key]):
-                        if idx > (len(state_stats[key]) - 1):  # WARNING: EXCEPT WITH HISTORY
+                    for idx, subjson in enumerate(parsed_json[key]):
+                        if idx > (len(subtree[key]) - 1):  # WARNING: EXCEPT WITH HISTORY
                             novelty = True
                             novelty_properties['trigger'] = [key, parsed_json[key]]
                             break
                         else:  # TODO: if subtree the same, add count, if not, insert? frankly list should become a tree/dict
-                            novelty, novelty_properties = self.check_novelty(state_stats[key][idx],
+                            novelty, novelty_properties = self.check_novelty(subtree[key][idx],
                                                                              parsed_json[key][idx])
                             # novelty_properties.update(temp_novelty_properties)
                 elif type(parsed_json[key][-1]) is list:  # list of lists - hardcoding this because die sequence only
-                    if state_stats[key]['length'] == len(parsed_json[key]):
+                    if subtree[key]['length'] == len(parsed_json[key]):
                         pass  # TODO balloch: this logic works but probably should be more rigorous
                         # print('No dice change updates')
                     else:
                         for idx, item in enumerate(parsed_json[key][-1]):
-                            if item not in state_stats[key]['data'][idx]:
+                            if item not in subtree[key]['data'][idx]:
                                 novelty = True
                                 novelty_properties['trigger'] = [key, parsed_json[key]]
                                 break
 
                             else:
-                                state_stats[key]['data'][idx][item] += 1
+                                subtree[key]['data'][idx][item] += 1
                 else:  # list of (str, bool int float)
-                    if tuple(parsed_json[key]) not in state_stats[key]:
+                    if tuple(parsed_json[key]) not in subtree[key]:
                         novelty = True
                         novelty_properties['trigger'] = [key, parsed_json[key]]
                         break
                     else:
                         continue
-                        # state_stats[key][tuple(gameboard[key])] += 1
+                        # subtree[key][tuple(parsed_json[key])] += 1
 
             else:  # (str, bool, int, float) # TODO balloch: untested
-                if parsed_json[key] not in state_stats[key]:
+                if parsed_json[key] not in subtree[key]:
                     novelty = True
                     novelty_properties['trigger'] = [key, parsed_json[key]]
                     break
                 else:
                     continue
-                    # state_stats[key][parsed_json[key]] += 1
+                    # subtree[key][parsed_json[key]] += 1
 
             if novelty == True:
                 # novelty_properties.insert()
@@ -201,7 +207,6 @@ class GameClone():
 
 def main():
     game_clone = GameClone()
-    state_stats = dict()
     for i in range(100):
         try:
             json_file = os.path.join('sample_json_data', 'data_' + '1' + '_' + str(i) + '.json')
@@ -209,13 +214,13 @@ def main():
             continue
         with open(json_file, 'r') as infile:
             data_dict_from_server = json.load(infile)
-        state_stats = game_clone.update_novelty_detector(state_stats, data_dict_from_server['current_gameboard'])
+        state_stats = game_clone.update_novelty_detector(data_dict_from_server['current_gameboard'])
     print(state_stats)
 
     bad_json_file = os.path.join('sample_json_data', 'bad_data_' + '1' + '_' + '10' + '.json')
     with open(bad_json_file, 'r') as badfile:
         bad_data_dict_from_server = json.load(badfile)
-    print(game_clone.check_novelty(state_stats, bad_data_dict_from_server['current_gameboard']))
+    print(game_clone.check_novelty(bad_data_dict_from_server['current_gameboard']))
 
 
             #### ONLY FOR CURRENT GAMEBOARD
